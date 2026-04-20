aa4d69e30d800fe4a4f51328190993c4de661e9a
Prithivi <prithivi@users.noreply.github.com>
2026-04-20 13:47:47 +0545

    feat: add POST endpoint for chat functionality with message streaming

---
+++ app/api/chat/route.ts
@@ -0,0 +1,27 @@
+import { UIMessage, convertToModelMessages, streamText } from "ai"
+import { google } from "@ai-sdk/google"
+
+export async function POST(request: Request) {
+  try {
+    const { messages }: { messages: UIMessage[] } = await request.json()
+
+    const result = streamText({
+      model: google("gemini-2.0-flash"),
+      messages: await convertToModelMessages(messages),
+    })
+
+    result.usage.then((usage) => {
+      console.log("Message Count: ", messages.length)
+      ;(console.log("Input token: ", usage.inputTokens),
+        console.log("Output token: ", usage.outputTokens),
+        console.log("Total token: ", usage.totalTokens))
+    })
+
+    return result.toUIMessageStreamResponse()
+  } catch (error) {
+    console.error("Error in POST /api/chat:", error)
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+    })
+  }
+}
